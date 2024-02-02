e744aaf30649da59784e443fcb52eaeb30dac1b0
Evgenii Istomin <ilonsisse@yandex.ru>
2024-02-02 21:14:22 +0300

    Reviewer function rate() added

--- learning_platform.py
+++ learning_platform.py
@@ -22,4 +22,14 @@ class Lecturer(Mentor):
 
 
 class Reviewer(Mentor):
-    pass
+
+     def rate(self, student, course, grade):
+        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+            if course in student.grades:
+                student.grades[course] += [grade]
+            else:
+                student.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
+
